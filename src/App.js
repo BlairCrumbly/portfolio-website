e560b742285557cd60d1e6076be81c8335e7ad6b
Blair Crumbly <blaircrumbly@gmail.com>
2025-02-22 03:13:21 -0500

    light and dark mode, skilllist started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
 import './App.css';
-import Hero from  './components/Hero'
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
+
 function App() {
   return (
-    <div>
-
-      <Hero/>
-    </div>
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import styles from '../styles/HeroStyles.css'
 import heroImg from '../assets/hero-img.png'
-import themeIcon from '../assets/sun.svg'
-import githubIcon from '../assets/github-light.svg'
-import linkedinIcon from '../assets/linkedin-light.svg'
-import twitterinIcon from '../assets/twitter-light.svg'
+import sun from '../assets/sun.svg'
+import moon from '../assets/moon.svg'
+import githubLight from '../assets/github-light.svg'
+import githubDark from '../assets/github-dark.svg'
+import linkedinLight from '../assets/linkedin-light.svg'
+import linkedinDark from '../assets/linkedin-dark.svg'
+import twitterLight from '../assets/twitter-light.svg'
+import twitterDark from '../assets/twitter-dark.svg'
 import CV from '../assets/cv.pdf'
+import {useTheme} from '../common/ThemeContext'
 
 function Hero() {
+    const {theme, toggleTheme} = useTheme ();
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+
+
   return (
     <section id='hero' className={styles.container}>
         <div className={styles.colorModeContainer}>
             <img className={styles.hero} src={heroImg} alt="profile picture of Blair Crumbly" 
             />
-            <img className={styles.colorMode} src={themeIcon} alt='Color Mode Icon'/>
+            <img className={styles.colorMode} src={themeIcon} alt='Color Mode Icon'
+            onClick={toggleTheme}
+            />
+            
             </div>
             <div className={styles.info}>
             <h1>Blair
@@ -32,7 +48,7 @@ function Hero() {
                     </a>
 
                     <a href='https://www.linkedin.com/in/blair-crumbly-36a317309/' target='_blank'>
-                    <img src={twitterinIcon}></img>
+                    <img src={twitterIcon}></img>
                     </a>
                 </span>
                 <p>With a passion for developing software and thinking outide of the box.</p>
